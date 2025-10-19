5d1d4e5428231a862918cd94602a969355cecafd
akinori-sumiya <aic201643s@gmail.com>
2025-10-19 10:47:22 +0900

    モデルとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_14_035625) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_19_014419) do
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

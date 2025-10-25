be93a7f8206ab74172ae5195ad86abee32efb867
akinori-sumiya <aic201643s@gmail.com>
2025-10-25 13:34:07 +0900

    ItemsControllerにcreateアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,21 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      # バリデーションに失敗した場合は、newテンプレートを再描画
+      render :new, status: :unprocessable_entity
+    end
   end
 end
 

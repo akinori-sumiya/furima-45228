bb38c00bbdda668fe6d534abe35f29479cdcf1c7
akinori-sumiya <aic201643s@gmail.com>
2025-10-25 11:09:39 +0900

    モデルの単体テストコードを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,11 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :authenticate_user!, only: [:new, :create]
 
   def index
   end
 
   def new
     @item = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
   end
 end
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  { Faker::Commerce.product_name }
+    info                  { Faker::Lorem.sentence }
+    category_id           { Faker::Number.between(from: 2, to: 11) }
+    sales_status_id       { Faker::Number.between(from: 2, to: 7) }
+    shipping_fee_payer_id { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id         { Faker::Number.between(from: 2, to: 48) }
+    scheduled_delivery_id { Faker::Number.between(from: 2, to: 4) }
+    price                 { Faker::Number.between(from: 300, to: 9_999_999) }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open(Rails.root.join('spec/fixtures/files/test_image.png')), filename: 'test_image.png',
+                        content_type: 'image/png')
+    end
   end
 end

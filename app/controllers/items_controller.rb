4293adabbe5093944ae0fe69c16a7239554dbe63
akinori-sumiya <aic201643s@gmail.com>
2025-10-25 09:19:47 +0900

    画像の保存を許可するストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ end
 
 private
 
-def message_params
-  params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+def item_params
+  params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_payer_id, :prefecture_id,
+                               :scheduled_delivery_id, :price).merge(user_id: current_user.id)
 end

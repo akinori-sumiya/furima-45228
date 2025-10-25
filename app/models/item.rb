6339519c6ce61186bb4f773a791d5482d902a37c
akinori-sumiya <aic201643s@gmail.com>
2025-10-25 09:59:14 +0900

    itemモデルにバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,25 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_payer
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+
+  # --- バリデーション ---
+
+  # 必須項目の存在チェック
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :info, presence: true
+
+  # ActiveHashを利用した関連付けのバリデーション
+  # ActiveHashの最初の項目（id: 1, name: '---'）はプレースホルダーのため選択不可
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_payer_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
+  # 価格のバリデーション: 必須、整数、かつ指定範囲内であること
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end

7d09bf959302c75be25ce0561b0c6ca96b647221
akinori-sumiya <aic201643s@gmail.com>
2025-11-10 20:53:28 +0900

    OrderAddressの正常系テストを追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe OrderAddress, type: :model do
       item = FactoryBot.create(:item)
       @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
     end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_address).to be_valid
+      end
+    end
   end
 end

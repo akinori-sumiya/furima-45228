e5ef00439f0d2d7aa1e73f2baadd1227214e05cb
akinori-sumiya <aic201643s@gmail.com>
2025-11-10 20:45:15 +0900

    OrderAddress のスペックのセットアップを完了

--- app/models/order_address_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe OrderAddress, type: :model do
-  describe '購入情報の保存' do
-    before do
-      @order_address = FactoryBot.build(:order_address)
-    end
-  end
-end
---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  describe '購入情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+    end
+  end
+end

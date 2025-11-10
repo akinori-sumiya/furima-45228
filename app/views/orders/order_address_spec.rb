20bc2b4d95ff7cf74e7ea4ee798b481949c17baa
akinori-sumiya <aic201643s@gmail.com>
2025-11-10 18:43:26 +0900

    OrderAddress Formオブジェクトをテストするためのファイルを作成

---
+++ app/views/orders/order_address_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  describe '購入情報の保存' do
+    before do
+      @order_address = FactoryBot.build(:order_address)
+    end
+  end
+end

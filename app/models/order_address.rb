63aac4a1a0c8f4452bc1e0ba125e97eec864951c
akinori-sumiya <aic201643s@gmail.com>
2025-11-11 21:47:10 +0900

    Revert "OrderAddressモデルのバリデーション修正"
    This reverts commit c0053fa24ee2191f3b2764c91eaa295cacce0646.

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,17 +2,15 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :token
 
-  # presence: true（必須）のバリデーションをまとめる
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :addresses
-    validates :token
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+    validates :token
   end
-  # prefecture_idは「---」でないこと（idが1でないこと）を検証
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save

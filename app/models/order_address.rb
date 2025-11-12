6733201d54e929f5cf3db78eeff77be25e60b1f7
akinori-sumiya <aic201643s@gmail.com>
2025-11-13 07:05:53 +0900

    order_address.rbの重複記入箇所修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,10 +7,8 @@ class OrderAddress
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :addresses
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
     validates :token
   end

3af3b017c6839f2501d75436fc7f66056e91e3df
akinori-sumiya <aic201643s@gmail.com>
2025-11-14 21:06:29 +0900

    郵便番号、電話番号の修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,12 +6,15 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :addresses
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
     validates :token
   end
+
+  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save

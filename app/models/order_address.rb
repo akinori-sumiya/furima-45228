94ca9565b0a0e42613de79ccd1a776a93674aec0
akinori-sumiya <aic201643s@gmail.com>
2025-11-14 21:17:29 +0900

    OrderAddress に postal_code と phone_number の必須バリデーションを追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -11,9 +11,9 @@ class OrderAddress
     validates :token
   end
 
-  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+  validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
 
-  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
 
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
